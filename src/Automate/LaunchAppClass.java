28bffcf3fb96b53219ee572f02aa4f3f7ac1a4d8
Priyanka <Priyanka@LAPTOP-411IDHFP>
2018-04-23 18:34:05 +0530

    COde till launch login page

--- src/Automate/LaunchAppClass.java
+++ src/Automate/LaunchAppClass.java
@@ -18,19 +18,23 @@ import org.testng.annotations.Test;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+;
 
-public class LaunchAppClass {
+public class LaunchAppClass{
 
-	private static RemoteWebDriver driver;
+	private static AndroidDriver driver;
+	//private static AndroidDriver andDriver;
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
 		
 		
 		   try {
-			   File classpathRoot = new File(System.getProperty("user.dir"));
+			    // Create path to open apk and launch tp install 
+			    File classpathRoot = new File(System.getProperty("user.dir"));
 				File appDir = new File(classpathRoot, "/Apps/Ebay/");
 				File app = new File(appDir, "ebayy.apk");
 
+				//Create Capabilities to launch app using package and launching activity
 				DesiredCapabilities capabilities = new DesiredCapabilities();
 				capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
 				capabilities.setCapability("deviceName", "Moto G5s Plus");
@@ -41,73 +45,22 @@ public class LaunchAppClass {
 				capabilities.setCapability("appActivity", "com.ebay.mobile.activities.MainActivity");
  
 			    //Create RemoteWebDriver instance and connect to the Appium server
-			    //It will launch the Calculator App in Android Device using the configurations
 			    //specified in Desired Capabilities
-			    driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+				//driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+				driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			    
+			    Thread.sleep(15000);
+			    
+			    //Click nutton of SIGN IN to redirect on login page 
+			    driver.findElementById("com.ebay.mobile:id/button_sign_in").click();
+			   // driver.findElement(By.id("com.ebay.mobile:id/button_sign_in")).click();
+			    
+			   // Thread.sleep(2000);
+			   // driver.findElementById("com.ebay.mobile:id/edttxt_username").sendKeys("saurabh");
+			    
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
-		
-		
-		
-	/*	driver = new AppiumDriver<>(service, desiredCapabilities)
-		//changes in code
-		driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);//<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
-		Thread.sleep(10000);
-		driver.quit();*/
 	}
-	
-/*	WebDriver driver;
-	 
-    @BeforeClass
-    public void setUp() throws MalformedURLException{
-        //Set up desired capabilities and pass the Android app-activity
-        //and app-package to Appium
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("BROWSER_NAME", "Android");
-        capabilities.setCapability("VERSION", "4.4.2");
-        capabilities.setCapability("deviceName","EmulatorS4");
-        capabilities.setCapability("platformName","Android");
- 
-        // This package name of your app (you can get it from apk info app)
-        capabilities.setCapability("appPackage", "com.android.calculator2");
- 
-        // This is Launcher activity of your app (you can get it from apk info app)
-        capabilities.setCapability("appActivity","com.android.calculator2.Calculator");
- 
-        //Create RemoteWebDriver instance and connect to the Appium server
-        //It will launch the Calculator App in Android Device using the configurations
-        //specified in Desired Capabilities
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
- 
-    @Test
-    public void testCal() throws Exception {
-        //locate the Text on the calculator by using By.name()
-        WebElement two=driver.findElement(By.name("2"));
-        two.click();
-        WebElement plus=driver.findElement(By.name("+"));
-        plus.click();
-        WebElement four=driver.findElement(By.name("4"));
-        four.click();
-        WebElement equalTo=driver.findElement(By.name("="));
-        equalTo.click();
-        //locate the edit box of the calculator by using By.tagName()
-        WebElement results=driver.findElement(By.className("android.widget.EditText"));
-        //Check the calculated value on the edit box
-        assert results.getText().toString().equals("6"):"Actual value is : "
-                +results.getText()+" did not match with expected value: 6";
-    }
- 
-    @AfterClass
-    public void teardown(){
-        //close the app
-        driver.quit();
-    }
-	
-	*/
-	
 }
