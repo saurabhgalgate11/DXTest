cccb39b7d90f79f76fb9e7d7f0346e056c0de3b0
Priyanka <Priyanka@LAPTOP-411IDHFP>
2018-04-23 22:49:36 +0530

    changes to list open and scroll

--- src/Automate/LaunchAppClass.java
+++ src/Automate/LaunchAppClass.java
@@ -2,6 +2,7 @@ package Automate;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -10,14 +11,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
+import org.openqa.selenium.remote.server.handler.interactions.touch.Down;
+import org.openqa.selenium.remote.server.handler.interactions.touch.Scroll;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 ;
 
 public class LaunchAppClass{
@@ -52,15 +56,40 @@ public class LaunchAppClass{
 			    Thread.sleep(15000);
 			    
 			    //Click nutton of SIGN IN to redirect on login page 
-			    driver.findElementById("com.ebay.mobile:id/button_sign_in").click();
-			   // driver.findElement(By.id("com.ebay.mobile:id/button_sign_in")).click();
+			    //driver.findElementById("com.ebay.mobile:id/button_sign_in").click();
 			    
-			   // Thread.sleep(2000);
-			   // driver.findElementById("com.ebay.mobile:id/edttxt_username").sendKeys("saurabh");
+
+			    driver.findElementById("com.ebay.mobile:id/search_box").click();
+			    
+			    Thread.sleep(2000);
+			    driver.findElementById("com.ebay.mobile:id/search_src_text").sendKeys("65-inch TV"+"\n");
+			 
+			   /* Thread.sleep(3000);
+			    AndroidElement list = (AndroidElement) driver.findElement(By.id("com.ebay.mobile:id/recycler"));
+		        MobileElement listGroup = list
+		                .findElement(MobileBy
+		                        .AndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView("
+		                                + "new UiSelector().text(\" List item:10\"));"));*/
+			    
+			    
+			   // driver;
+			    
+		        Thread.sleep(10000);
+			    List<WebElement> list2 = driver.findElements(By.id("com.ebay.mobile:id/recycler"));
+			    System.out.println("List SIze-->"+list2.size());
+			    Thread.sleep(100);
+			   
+			    
+			    
 			    
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	public static int getRandom(int max){ // 
+		return (int) (Math.random()*max); //incorrect always return zero return (int) (Math.random()*max); }
+	}
+
 }
